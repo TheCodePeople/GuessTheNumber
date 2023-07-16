94dee2140c0866e29bb7a9772862efcdbd6b8f18
abbas-abdalhusain-yosif <abs_abdal@yahoo.com>
2023-07-16 09:09:39 -0700

    Solve Task : GuessTheNumber / date: 16/7/2023

---
+++ app.js
@@ -0,0 +1,37 @@
+function generateRandomNumber()
+{
+    let randomn = Math.floor(Math.random() * 100);
+    alert(randomn);
+    return randomn;
+}
+function getUserGuess()
+{
+    return parseInt(prompt("ادخل رقم بين 1 و 100"));
+
+}
+function compareNumbers(userGess , actualNumber)
+{
+    if(userGess > actualNumber)
+    {
+        alert("الرقم كبير");
+        return false;
+    }else if(userGess < actualNumber){
+        alert("الرقم صغير");
+        return false;
+    }else if(userGess == actualNumber)
+    {
+        return true;
+    }
+}
+function playGame()
+{
+    let gen=generateRandomNumber();
+if (compareNumbers(getUserGuess() , gen)  == true)
+{
+    
+alert("لقد نجحت");
+}
+}
+while (true){
+playGame();
+}
