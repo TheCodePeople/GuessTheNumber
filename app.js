9f61803b335f0b6a45a8bbe201e5adb4491bfe42
KJ-MU <gullssea5@gmail.com>
2024-02-12 18:24:27 +0300

    function task complete

---
+++ app.js
@@ -0,0 +1,33 @@
+function generateRandomNumber(){
+let num=Math.floor(Math.random()*100+1);
+console.log(num);
+    return num ;
+}
+
+function getUserGuess(){
+    let guess=prompt("Guess a number between 1 and 100.");
+    return guess;
+}
+
+function compareNumbers(userGuess, actualNum){
+if(userGuess===actualNum){
+    console.log("Correct!!");
+    return "true";
+}
+else if (userGuess>actualNum){
+    console.log("Too high!!!");
+    return "false";
+}
+else if (userGuess<actualNum){
+        console.log("Too low!!!");
+        return "false";
+}
+}
+
+function playGame(){
+    let randomNum = generateRandomNumber();
+    let guess=getUserGuess();
+    console.log(compareNumbers(guess, randomNum));
+}
+
+playGame();
